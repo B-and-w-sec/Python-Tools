b64d37f58f4977a54377213b25b03b31b62b00cc
B-and-w-sec <33036569+B-and-w-sec@users.noreply.github.com>
2020-10-26 09:40:15 +0530

    Create reconbot1.py

---
+++ reconbot1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import requests
+import json
+
+api_key="839983611:AAEHnwgYWnPa-Rt9GhTRWQgnXtoF0xO0-fk"
+URL = "https://api.telegram.org/bot{0}/".format(api_key)
+
+def get_url(url):
+	res = requests.get(url)
+	content = res.content.decode("utf-8")
+	return content
+
+def getUpdates(offset=None):
+	url = URL + "getUpdates?timeout=100" 
+	if offset:
+		url = url + "&offset={}".format(offset+1)
+	updates= get_url(url)
+	upd = json.loads(updates)
+	return upd
+
+def main():
+	update_id = None
+	while True:
+		data = getUpdates(offset=update_id)
+		results = data['result']
+		if results:
+			for i in data['result']:
+				update_id = i['update_id']
+				chatid=i['message']['from']['id']
+				usr=i['message']['from']['username']
+				message = "Hello,"+usr
+				url = URL+ "sendMessage?chat_id={0}&text={1}".format(chatid,message)
+				get_url(url)
+				print("message sent!")
+
+main()
